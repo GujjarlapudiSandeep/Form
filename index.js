c3c37241505c5ba064a48cddcf1237632683cb60
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-09-10 17:43:30 +0530

    added js

---
+++ index.js
@@ -0,0 +1,13 @@
+document.querySelector("#formTag").addEventListener("submit", myFunction);
+
+function myFunction() {
+  // console.log(myFunction);
+  event.preventDefault();
+  let firstName = document.querySelector(".firstname").value;
+  let lastName = document.querySelector(".lastname").value;
+  let email = document.querySelector("#email").value;
+  let hobbies = document.querySelector("#hobbies").value;
+  let birthYear = document.querySelector("#birthyear").value;
+  let gender = document.querySelector("#gender").value;
+  console.log(firstName, lastName, email, hobbies, birthYear, gender);
+}
