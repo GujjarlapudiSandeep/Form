6d6fc9b57dbb382fd12ec28dc8c1bb199c60f854
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-09-25 16:29:14 +0530

    updated

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.querySelector("#formTag").addEventListener("submit", myFunction);
 
 function myFunction() {
-  // event.preventDefault(); // Prevent the form from submitting
+  event.preventDefault(); // Prevent the form from submitting
   // Get the values from the form fields
   let firstName = document.querySelector(".firstname").value;
   let lastName = document.querySelector(".lastname").value;
   let email = document.querySelector("#email").value;
-  let hobbies = document.querySelector("#hobbies").value;
+  let jobsearch = document.querySelector("#jobsearch").value;
   let birthYear = document.querySelector("#birthyear").value;
   // Get the selected gender
   let gender = document.querySelector("#gender").value;
   // Log the form values to the console
-  console.log(firstName, lastName, email, hobbies, birthYear, gender);
+  console.log(firstName, lastName, email, jobsearch, birthYear, gender);
   // Check if all fields are filled, then display an alert
   if (firstName && lastName && email && hobbies && birthYear && gender) {
     alert("Submitted Successfully!"); // Show success alert
